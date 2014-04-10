49d5bec8ff67463f7c64de01a01aef3f66aae6da
Robert L. Carpenter <robacarp@gmail.com>
2014-04-09 18:53:18 -0600

    A simple cookbook to create users on a system

---
+++ cookbooks/user_base/metadata.rb
@@ -0,0 +1,7 @@
+name             'user_base'
+maintainer       'Robert L Carpenter'
+maintainer_email 'robert@robacarp.com'
+license          'MIT'
+description      'Installs/Configures users on a linux system after a debian pattern'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/user_base/recipes/default.rb
@@ -0,0 +1,53 @@
+users = []
+data_bag("users").each do |user|
+  users.push data_bag_item("users", user).to_hash
+end
+
+users.each_with_index do |user, i|
+  user user['id'] do
+    action :create
+    username user['id']
+    password user['password']
+    home user['home']
+    uid user['uid']
+  end
+
+  group user['uid'] do
+    action :create
+    append false
+    members user['id']
+    gid user['gid']
+    group_name user['id']
+  end
+
+  directory "#{user['home']}/.ssh" do
+    recursive true
+  end
+
+  public_keys = ""
+  if user['public_key_url']
+    remote_file "#{user['home']}/.ssh/authorized_keys" do
+      source user['public_key_url']
+      checksum user['checksum'] if user['checksum']
+
+      action :create_if_missing
+      owner user['uid']
+      group user['gid']
+      backup false
+    end
+  end
+
+  # mayhaps allow some sort of data-bag based public key?
+  # file "#{user['home']}/.ssh/authorized_keys" do
+  # end
+end
+
+sudoers = users.select {|u| u['groups'].include? "sudo"}
+               .map    {|u| u['id'] }
+               .join(',')
+
+group "sudo" do
+  append true
+  group_name "sudo"
+  members sudoers
+end
