db6f9aa92cb7598ebd6cf3cca5c76ce97a0f9758
Robert L. Carpenter <robacarp@gmail.com>
2015-04-27 01:45:21 -0600

    default ruby is new

--- cookbooks/ruby/attributes/default.rb
+++ cookbooks/ruby/attributes/default.rb
@@ -1,7 +1,5 @@
-default[:ruby][:version] = '2.0.0-p247'
+default[:ruby][:version] = '2.2.2'
 default[:ruby][:basename] = "ruby-#{node[:ruby][:version]}"
 default[:ruby][:tar_filename] = "#{node[:ruby][:basename]}.tar.gz"
-default[:ruby][:url] = "http://ftp.ruby-lang.org/pub/ruby/stable/#{node[:ruby][:tar_filename]}"
-default[:ruby][:sha] = '3e71042872c77726409460e8647a2f304083a15ae0defe90d8000a69917e20d3'
-
-
+default[:ruby][:url] = "http://cache.ruby-lang.org/pub/ruby/2.2/#{node[:ruby][:tar_filename]}"
+default[:ruby][:sha] = '5ffc0f317e429e6b29d4a98ac521c3ce65481bfd22a8cf845fa02a7b113d9b44'
